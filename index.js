9725e84f3ec0cec0dbdf5286e3212c587ba1d125
Simon Townsend <stowns3@gmail.com>
2015-09-12 12:34:10 -0500

    parsing changes

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-var cookies = {},
-    _ = require('lodash');
+import _ from 'lodash';
 
-cookies.getCookie = function (name) {
-  // get our session AND base64 decode the value
-  if (_.isEmpty(document.cookie)) { return null; }
+class Cookies {
 
-  var cookie = new Buffer(document.cookie.replace("session=",""), 'base64').toString();
-  
-  if (!cookie) { return null; };
+  constructor(config) {
+    this.conf = config;
+  }
 
-  var parsed = JSON.parse(cookie);
-  return (parsed[name] != null) ? parsed[name] : null;
+  getCookie(name) {
+    // get our session AND base64 decode the value
+    if (_.isEmpty(document.cookie)) { return null; }
+
+    var cookieObj = this.parseCookie();
+    if (_.isNull(cookieObj)[name]) { return null; }
+
+    var cookie = this.conf.base64 ? new Buffer(cookieObj[name], 'base64').toString() : cookieObj[name];
+    var parsed = JSON.parse(cookie);
+    
+    return parsed;
+  }
+
+  parseCookie(){
+    str = document.cookie.split(', ');
+    var result = {};
+    for (var i = 0; i < str.length; i++) {
+        var cur = str[i].split('=');
+        result[cur[0]] = cur[1];
+    }
+
+    return result;
+  }
 }
 
-module.exports = cookies;
+module.exports = Cookies;
